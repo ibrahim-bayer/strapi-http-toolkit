a5bbbb25a9c7a98fd164dbcfacb7ff94e6fb61e3
ibrahim.bayer <ibrahimbayer@gmail.com>
2025-06-02 16:50:49 +0700

    added search parameters

--- src/FetchRequestParameters.ts
+++ src/FetchRequestParameters.ts
@@ -1,23 +1,22 @@
-import { FilterOptions } from "./FilterOptions";
 import { CrudRequestModel } from "./CrudRequestModel";
+import { FilterOptions } from "./FilterOptions";
 import { PopulateOptions } from "./PopulateOptions";
+import { SearchParameters } from "./SearchParameters";
 
 export interface FetchRequestParameters<X> {
   method: string;
   headers?: HeadersInit;
   cache?: RequestCache;
-  parameters?: {
-    [key: string]: string[] | string;
-  };
+  parameters?: SearchParameters;
   filters?: FilterOptions<X>;
   pagination?: {
     start?: number;
     limit?: number;
-    page?:number
-    pageSize?:number
+    page?: number;
+    pageSize?: number;
   };
   relations?: PopulateOptions<X>;
   sort?: string[];
   body?: CrudRequestModel<X>;
-  customBody?: object
+  customBody?: object;
 }
