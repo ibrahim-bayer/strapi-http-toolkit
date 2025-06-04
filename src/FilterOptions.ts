c5c67cf85ec1719d2a7ad4068e5ffebe01606e59
ibrahim.bayer <ibrahimbayer@gmail.com>
2025-06-04 16:44:14 +0700

    v1.0.7-nested filter options

--- src/FilterOptions.ts
+++ src/FilterOptions.ts
@@ -1,44 +1,45 @@
 export type FilterCondition<T> = {
-    $eq?: T;
-    $ne?: T;
-    $in?: T[];
-    $nin?: T[];
-    $null?: boolean;
-    $lt?: T;
-    $lte?: T;
-    $gt?: T;
-    $gte?: T;
-    $contains?: T extends string ? string : never;
-    $ncontains?: T extends string ? string : never;
-    $startsWith?: T extends string ? string : never;
-    $endsWith?: T extends string ? string : never;
+  $eq?: T;
+  $ne?: T;
+  $in?: T[];
+  $nin?: T[];
+  $null?: boolean;
+  $lt?: T;
+  $lte?: T;
+  $gt?: T;
+  $gte?: T;
+  $contains?: T extends string ? string : never;
+  $ncontains?: T extends string ? string : never;
+  $startsWith?: T extends string ? string : never;
+  $endsWith?: T extends string ? string : never;
 };
 
-type FilterConditionOptions<T> = {
-    [K in keyof T]?: T[K] extends string
-      ? FilterCondition<string>
-      : T[K] extends number
-      ? FilterCondition<number>
-      : T[K] extends boolean
-      ? FilterCondition<boolean>
-      : T[K] extends bigint
-      ? FilterCondition<bigint>
-      : T[K] extends Date
-      ? FilterCondition<Date>
-      : T[K] extends (infer U)[]
-      ? FilterConditionOptions<U> | FilterCondition<U>
-      : NonNullable<T[K]> extends object
-      ? FilterConditionOptions<NonNullable<T[K]>> | FilterCondition<NonNullable<T[K]>>
-      : FilterCondition<T[K]>; // Alt alanlar desteklenir.
-  };
+export type FilterConditionWithId<T> = T extends { documentId: infer ID }
+  ? FilterCondition<T> | FilterCondition<ID>
+  : FilterCondition<T>;
 
-// Array condition to handle filters within arrays
-type FilterArrayCondition<T> = {
-    $elemMatch?: FilterConditionOptions<T> | FilterCondition<T>;
+type FilterConditionOptions<T> = {
+  [K in keyof T]?: T[K] extends string
+    ? FilterCondition<string>
+    : T[K] extends number
+    ? FilterCondition<number>
+    : T[K] extends boolean
+    ? FilterCondition<boolean>
+    : T[K] extends bigint
+    ? FilterCondition<bigint>
+    : T[K] extends Date
+    ? FilterCondition<Date>
+    : T[K] extends (infer U)[]
+    ? FilterConditionOptions<U> | FilterCondition<U>
+    : NonNullable<T[K]> extends object
+    ?
+        | FilterConditionOptions<NonNullable<T[K]>>
+        | FilterConditionWithId<NonNullable<T[K]>>
+    : FilterCondition<T[K]>; // Alt alanlar desteklenir.
 };
 
 // Main type to apply filters to a model
 export type FilterOptions<T> = FilterConditionOptions<T> & {
-    $and?: FilterOptions<T>[]; 
-    $or?: FilterOptions<T>[];  
-  };
+  $and?: FilterOptions<T>[];
+  $or?: FilterOptions<T>[];
+};
